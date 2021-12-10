73f53d095d4ecc94fd1dbbe0c2b2f05e9d31cc99
Brian Prost <brian.s.prost@gmail.com>
2021-12-10 12:53:56 -0500

    wine makes everything better, including modularity

--- homework_4.py
+++ homework_4.py
@@ -1,3 +1,9 @@
 # Brian Prost
 # homework_4.py
 
+from wine import wine
+
+
+
+if __name__ == "__main__":
+    main_menu()
--- wine/wine.py
+++ wine/wine.py
@@ -1,14 +1,14 @@
 import json
 
-with open('wines.json') as f:
-    wine_db = json.load(f)
-
-wine_to_rate = wine_db["white"]["sauvignon blanc"]["brian's rating"]
-
 # TODO: would love to make this a lambda function, but i'm dumb
 def get_wine_stars(wine_rating):
-    if wine_rating is -1:
+    # TODO: move to DynamoDB
+    with open('wine/wines.json') as f:
+        wine_db = json.load(f)
+
+    if wine_rating < 0:
         return "He hasn't tried this one yet! Feel free to buy him one."
+
     num_stars = round(wine_rating * 5)
     star_rating = ""
     for i in range(num_stars):
@@ -17,4 +17,6 @@ def get_wine_stars(wine_rating):
         star_rating += "\u2606"
     return star_rating
 
-print(get_wine_stars(wine_to_rate))
+if __name__ == "__main__":
+    cab_rating = get_wine_stars(1)
+    print(f"Cabernet Sauvignon is always {cab_rating}")
