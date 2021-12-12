63305f7754d4094c1b4943ef8c85aa6db7e2e1c6
Brian Prost <brian.s.prost@gmail.com>
2021-12-12 17:35:06 +0000

    implemented upload method for COVID data

--- covid/upload_covid_data_to_dynamodb.py
+++ covid/upload_covid_data_to_dynamodb.py
@@ -1,20 +1,26 @@
 import boto3
 import json
 
-joke_table_dynamodb = boto3.resource('dynamodb').Table('NytCovidDataDec10')
+JOKE_TABLE_DYNAMODB = boto3.resource('dynamodb').Table('NytCovidDataDec10')
 
-with open('covid/nyt_us_states.csv', 'r') as f:
-    next(f)
-    for line in f:
-        state, cases, deaths = line.rstrip().split(',')
-        upload_to_dynamodb(state, cases, deaths)
+def main():
+
+
+    with open('nyt_us_states.csv', 'r') as f:
+        next(f)
+        for line in f:
+            state, cases, deaths = line.rstrip().split(',')
+            upload_to_dynamodb(state, cases, deaths)
 
 
 def upload_to_dynamodb(state, cases, deaths):
-    joke_table_dynamodb.put_item(
+    JOKE_TABLE_DYNAMODB.put_item(
         Item={
             'state': state,
             'cases': int(cases),
             'deaths': int(deaths)
         }
     )
+
+if __name__ == "__main__":
+    main()
