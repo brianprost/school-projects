bc1230060e37e4f23cc4e3f0d795ec6c063f67c6
Brian Prost <brian.s.prost@gmail.com>
2021-12-12 18:26:24 +0000

    upload data is now a python method

--- covid/upload_covid_data_to_dynamodb.py
+++ covid/upload_covid_data_to_dynamodb.py
@@ -1,26 +1,52 @@
 import boto3
 import json
 
-COVID_TABLE_DYNAMODB = boto3.resource('dynamodb').Table('NytCovidDataDec10')
-
 def main():
+    create_db()
+    upload_to_db()
+
+def create_db():
+    # TODO: is the next 2 lines the same as boto3.resource('dynamodb').Table('NytCovidDataDec10') ?
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.create_table(
+        TableName="NytCovidDataDec10",
+        KeySchema=[
+            {
+                "AttributeName": "state",
+                "KeyType": "HASH"
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                "AttributeName": "state",
+                "AttributeType": "S"
+            },
+        ],
+        ProvisionedThroughput={
+            "ReadCapacityUnits": 5,
+            "WriteCapacityUnits": 5
+        }
+    )
+    print("Creating table. Please wait...")
+    table.wait_until_exists()
+    print("Table created! Adding data now...")
 
 
+def upload_to_db():
+    covid_table_dynamodb = boto3.resource('dynamodb').Table('NytCovidDataDec10')
     with open('nyt_us_states.csv', 'r') as f:
         next(f)
         for line in f:
             state, cases, deaths = line.rstrip().split(',')
-            upload_to_dynamodb(state, cases, deaths)
+            covid_table_dynamodb.put_item(
+                Item={
+                    'state': state,
+                    'cases': int(cases),
+                    'deaths': int(deaths)
+                }
+            )
+    print("Data added!")
 
-
-def upload_to_dynamodb(state, cases, deaths):
-    COVID_TABLE_DYNAMODB.put_item(
-        Item={
-            'state': state,
-            'cases': int(cases),
-            'deaths': int(deaths)
-        }
-    )
 
 if __name__ == "__main__":
     main()
