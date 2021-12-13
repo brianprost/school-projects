aeea63761033ea01cf287470644af28d8d0eaed7
Brian Prost <brian.s.prost@gmail.com>
2021-12-13 16:01:13 +0000

    dad jokes are now from DynamoDB

--- dad_jokes/dad_jokes.py
+++ dad_jokes/dad_jokes.py
@@ -1,13 +1,16 @@
-import json
+import boto3
 import random
 
 def get_dad_joke():
-    # TODO: move to DynamoDB
-    with open('dad_jokes/jokes.json') as f:
-        joke_db = json.load(f)
+    dynamodb_r = boto3.resource('dynamodb')
+    dad_jokes = dynamodb_r.Table('DadJokesDb')
 
-    joke = random.choice(joke_db)
+    joke_id = random.randint(0,dad_jokes.item_count)
+
+    joke_obj = dad_jokes.get_item(Key={'joke_id': joke_id})
+    joke = joke_obj['Item']['joke']
+
     return joke
 
 if __name__ == "__main__":
-    print("What did the ocean say to the beach? Nothing, it just waved.")
+    get_dad_joke()
