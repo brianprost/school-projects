dfb3108dd9fccc3d74be1a36bd949ff7793c3b21
Brian Prost <brian.s.prost@gmail.com>
2021-12-13 18:09:04 +0000

    dad jokes now uses requests to api to awsLambda

--- dad_jokes/dad_jokes.py
+++ dad_jokes/dad_jokes.py
@@ -1,23 +1,11 @@
-import boto3
-import random
-
-
-def menu():
-    dad_joke = get_dad_joke()
-
+import requests
 
 def get_dad_joke():
-    dynamodb_r = boto3.resource('dynamodb')
-    dad_jokes = dynamodb_r.Table('DadJokesDb')
-
-    joke_id = random.randint(0,dad_jokes.item_count)
-
-    joke_obj = dad_jokes.get_item(Key={'joke_id': joke_id})
-    joke = joke_obj['Item']['joke']
-
-    return joke
+    api_url = "https://zhb2ups653.execute-api.us-east-1.amazonaws.com/default/DadJokeBot"
+    response = requests.get(api_url)
+    print(response.json())
 
 
 if __name__ == "__main__":
     # print(get_dad_joke())
-    menu()
+    get_dad_joke()
---
+++ dad_jokes/dad_jokes_lambda_function.py
@@ -0,0 +1,25 @@
+import boto3
+import json
+import random
+
+def lambda_handler(event, context):
+    joke = get_dad_joke()
+    return {
+        'statusCode': 200,
+        'body': json.dumps(joke)
+    }
+
+
+def get_dad_joke():
+    dynamodb_r = boto3.resource('dynamodb')
+    dad_jokes = dynamodb_r.Table('DadJokesDb')
+
+    joke_id = random.randint(0,dad_jokes.item_count)
+
+    joke_obj = dad_jokes.get_item(Key={'joke_id': joke_id})
+    joke = joke_obj['Item']['joke']
+
+    return joke
+
+    joke = random.choice(joke_db)
+    return joke
