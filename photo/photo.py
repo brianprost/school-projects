26b3df88b8e69a5f23e992d21167ef3f4690eec2
Brian Prost <brian.s.prost@gmail.com>
2021-12-14 16:42:12 +0000

    image now downloads and then uploads to s3

--- photo/photo.py
+++ photo/photo.py
@@ -1,4 +1,5 @@
 import boto3
+import os
 import cv2
 import sys
 from pathlib import Path
@@ -6,34 +7,31 @@ import requests
 import re
 import shutil
 
-def lambda_event(event, context):
-    pass
-
 
 def menu():
     image_url = get_image_url()
-    
+
 
     # lambda from here on out
     image_from_internet = download_image_from_internet(image_url)
-    # upload_to_s3(image_from_internet)
-    if not image_from_internet[0]:
-        # implement
-        return 404
-    image_directory = "photo/img/"
-    image_path = image_directory + "a_famous_photo.jpeg"
-    image_name = Path(image_path).stem
-    image = cv2.imread(image_path)
-    image_grayed = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    faces = detect_faces(image_grayed)
+    # if not image_from_internet[0]:
+    #     # implement
+    #     return 404
+    # image_directory = "photo/img/"
+    # image_path = image_directory + "a_famous_photo.jpeg"
+    # image_name = Path(image_path).stem
+    # image = cv2.imread(image_path)
+    # image_grayed = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    for (x, y, w, h) in faces:
-        cv2.rectangle(image, (x, y), (x+w, y+h), (20, 9, 229), 4)
+    # faces = detect_faces(image_grayed)
 
-    status = cv2.imwrite(
-        f"{image_directory}{image_name}_faces_detected.jpeg", image)
-    print("Image faces_deteced.jpg written to filesystem: ", status)
+    # for (x, y, w, h) in faces:
+    #     cv2.rectangle(image, (x, y), (x+w, y+h), (20, 9, 229), 4)
+
+    # status = cv2.imwrite(
+    #     f"{image_directory}{image_name}_faces_detected.jpeg", image)
+    # print("Image faces_deteced.jpg written to filesystem: ", status)
 
 
 def get_image_url():
@@ -49,23 +47,26 @@ def get_image_url():
             r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 
         if re.match(image_url_regex, image_url) is not None:
-                return image_url                
+                return image_url
         print("Invalid URL. Please enter the full web address. ex: https://website.com/image/my_image.jpeg")
 
 
 def download_image_from_internet(image_url):
     filename = Path(image_url).name
     response = requests.get(image_url)
+
     if response.status_code != 200:
         return [False, "There was an error accessing your image.", None]
     response.raw.decode_content = True
-    s3 = boto3.resource('s3')
-    with open(filename, 'rb') as data:
-        s3.upload_fileobj(data, 'edu.umgc.sdev400.', filename)
 
-    with open(filename) as f:
+    with open(filename,'wb') as f:
         shutil.copyfileobj(response.raw, f)
-        return [True, "File successfully downloaded", filename]
+        
+    s3 = boto3.resource('s3')
+    s3.meta.client.upload_file(filename, 'edu.umgc.sdev400.prost.homework4', filename)
+    
+    os.remove(filename)
+    return [True, "File successfully downloaded", filename]
 
 
 
@@ -82,10 +83,6 @@ def detect_faces(image):
     )
     print("Found {0} Faces!".format(len(faces)))
     return faces
-
-
-def filter_pic():
-    pass
 
 
 if __name__ == "__main__":
