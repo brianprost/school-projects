d5e92f2ee82abcb1e601b23dc1d3f0da37af4b09
Brian Prost <brian.s.prost@gmail.com>
2021-12-14 11:10:40 -0500

    initial photo processing work

--- photo/photo.py
+++ photo/photo.py
@@ -1,9 +1,25 @@
+import boto3
 import cv2
 import sys
 from pathlib import Path
+import requests
+import re
+import shutil
+
+def lambda_event(event, context):
+    pass
 
 
 def menu():
+    image_url = get_image_url()
+    
+
+    # lambda from here on out
+    image_from_internet = download_image_from_internet(image_url)
+    # upload_to_s3(image_from_internet)
+    if not image_from_internet[0]:
+        # implement
+        return 404
     image_directory = "photo/img/"
     image_path = image_directory + "a_famous_photo.jpeg"
     image_name = Path(image_path).stem
@@ -20,8 +36,38 @@ def menu():
     print("Image faces_deteced.jpg written to filesystem: ", status)
 
 
-def get_pic():
-    pass
+def get_image_url():
+    while True:
+        image_url = input("Enter an image URL: ")
+
+        image_url_regex = re.compile(
+            r'^(?:http|ftp)s?://'
+            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'
+            r'localhost|'
+            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
+            r'(?::\d+)?'
+            r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
+        if re.match(image_url_regex, image_url) is not None:
+                return image_url                
+        print("Invalid URL. Please enter the full web address. ex: https://website.com/image/my_image.jpeg")
+
+
+def download_image_from_internet(image_url):
+    filename = Path(image_url).name
+    response = requests.get(image_url)
+    if response.status_code != 200:
+        return [False, "There was an error accessing your image.", None]
+    response.raw.decode_content = True
+    s3 = boto3.resource('s3')
+    with open(filename, 'rb') as data:
+        s3.upload_fileobj(data, 'edu.umgc.sdev400.', filename)
+
+    with open(filename) as f:
+        shutil.copyfileobj(response.raw, f)
+        return [True, "File successfully downloaded", filename]
+
+
 
 
 def detect_faces(image):
